6abd7e6a233f984e082bbdb6f376fd86dbd9d6a0
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-01 19:54:09 -0500

    adding div to sidebar

--- portfolio/src/components/Layout/index.js
+++ portfolio/src/components/Layout/index.js
@@ -3,9 +3,14 @@ import './index.scss';
 
 const Layout = () => {
     return (
-        <>
+        <div className="App">
             <Sidebar />
-        </>
+            <div className='page'>
+                <span className='tags top-tags'>&lt;body&gt;</span>
+
+                <Outlet />
+            </div>
+        </div>
     )  
 }
 
--- portfolio/src/components/Sidebar/index.js
+++ portfolio/src/components/Sidebar/index.js
@@ -3,6 +3,10 @@ import './index.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+    faLinkedin,
+    faGithub
+}  from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => (
