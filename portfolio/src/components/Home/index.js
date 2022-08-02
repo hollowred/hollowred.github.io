5f385255ad9b8a2782423b93147ed3c39b8929b5
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-02 15:34:57 -0500

    CSS container

---
+++ portfolio/src/components/Home/index.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assests/images/logo-s.png'
+
+
+const Home = () => {
+    
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+              <h1>Hi, <br /> I'm
+              <img src={LogoTitle} alt="developer" />
+              salem<br />
+              web developer
+              </h1>   
+              <h2> Full Stack Enginer</h2>   
+              <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home
