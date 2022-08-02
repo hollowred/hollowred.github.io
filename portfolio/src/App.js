0c3161e1fcb53e463673786dd50a3b6386f8c013
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-02 14:35:19 -0500

    Home

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -8,7 +8,9 @@ const App = () => {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
     </Routes>
     {/* <div>
       <h1>Hello!</h1>
