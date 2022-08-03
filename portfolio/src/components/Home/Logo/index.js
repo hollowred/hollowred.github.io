58eb001ca8ebd9fb8404d1e7066470dcfc960d68
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-03 14:47:10 -0500

    complete logo animation

--- portfolio/src/components/Home/Logo/index.js
+++ portfolio/src/components/Home/Logo/index.js
@@ -1,17 +1,45 @@
 import { useEffect, useRef } from 'react'
 import './index.scss';
 import LogoS from '../../../assests/images/logo-s.png'
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import gsap from 'gsap-trial'
 
 
-const Logo =() => {
-    
-
+const Logo =() => { 
     const bgRef = useRef()
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
+
+    useEffect(() =>{
+        gsap.registerPlugin(DrawSVGPlugin)
+
+        gsap.timeline()
+        .to(bgRef.current, {
+            duraion: 1,
+            opacity: 1,
+        })
+        .from(outlineLogoRef.current, {
+            drawSVG: 0,
+            duration: 20,
+        })
+
+        gsap.fromTo(
+            solidLogoRef.current,
+            {
+                opacity: 0,
+            },
+            {
+                opscity: 1,
+                delay: 4,
+                duration: 4,
+            }
+        )
+    }, [])
+
+
     return (
         <div className='logo-container'>
-            <img className='solid-logo' src={LogoS} alt="S" />
+            <img ref={solidLogoRef} className='solid-logo' src={LogoS} alt="S" />
             <svg
         width="559pt"
         height="897pt"
