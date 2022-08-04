3caa1578ed01dd4f7a8b1df352b82671ed4cc09d
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-03 20:08:46 -0500

    pacman

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,8 +1,10 @@
 import './index.scss'
-import AnimatedLeters from '../AnimatedLetters'
+import AnimatedLetters from '../AnimatedLetters'
 import { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngular, faCss3, faGitAlt, faHtml5, faJsSquare, faReact } from '@fortawesome/free-brands-svg-icons'
+import Loader from 'react-loaders'
+
 
 const About = () => {   
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -14,6 +16,7 @@ const About = () => {
     }, [])
 
     return (
+        <>
         <div className='container about-page'>
             <div className='text-zone'>
                 <h1>
@@ -48,6 +51,8 @@ const About = () => {
                 </div>
             </div>
         </div>
+        <Loader type="pacman" />
+       </> 
     )
 }
 
