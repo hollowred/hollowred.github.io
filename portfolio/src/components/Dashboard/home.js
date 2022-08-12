88dd30d193149ccc3dcb3ed858e3a80752bdbac3
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-12 00:04:13 -0500

    dashboard update

--- portfolio/src/components/Dashboard/home.js
+++ portfolio/src/components/Dashboard/home.js
@@ -1,13 +1,30 @@
+import { useRef } from 'react';
+import { auth, storage } from '../../firebase'
+import { ref } from 'firebase/storage'
 
 const Home = () => {
+    const form = useRef();
+
+    const submitPortfolio = (e) => {
+        e.preventDefault();
+        const name = form.current[0]?.value;
+        const description = form.current[1]?.value;
+        const url = form.current[2]?.value;
+        const iamge = form.current[3]?.files[0];
+
+        const storageRef = ref(storage, `portfolio/${image.name}`)
+    }
+
     return (
         <div className="dashboard">
 
-            <form>
+            <form ref={form} onSubmit={submitPortfolio}>
                 <p><input type="text" placehplder="Name" /></p>
                 <p><textarea placehplder="Description" /></p>
                 <p><input type="text" placehplder="Url" /></p>
                 <p><input type="file" placehplder="Image" /></p>
+                <button type="submit">Submit</button>
+                <button onClick={() => auth.signOut()}>Sign out</button>
             </form>
         </div>
     )
