5f95380a85e1c056ad5d668728b6a0fb2a56a1a1
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-08-12 00:35:52 -0500

    portfolio complete

--- portfolio/src/components/Portfolio/index.js
+++ portfolio/src/components/Portfolio/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters";
 import "./index.scss"
-import portfolioDate from '../../data/portfolio.json'
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../../firebase";
 
@@ -66,7 +65,7 @@ const Portfolio = () => {
                       idx={15} 
                  />
                 </h1>
-                <div>{renderPortfolio(portfolioDate.portfolio)}</div>
+                <div>{renderPortfolio(portfolio)}</div>
              </div>
             <Loader type="pacman" />
         </>
