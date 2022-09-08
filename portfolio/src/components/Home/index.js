1716aecacf04dbbe6341ade531c81f3bfca5eca6
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-08 14:16:58 -0500

    hello

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -27,13 +27,13 @@ const Home = () => {
                 <br /> 
                 <span className={`${letterClass} _13`}>I</span> 
                 <span className={`${letterClass} _14`}>'m</span>  
-              <AnimatedLetters letterClass={letterClass}
+              {/* <AnimatedLetters letterClass={letterClass}
                strArray={nameArray}
                idx={15}  />
               <br />
               <AnimatedLetters letterClass={letterClass}
                strArray={jobArray}
-               idx={22}  />
+               idx={22}  /> */}
               </h1>   
               <h2> Full Stack Enginer</h2>   
               <Link to="/contact" className="flat-button">CONTACT ME</Link>
