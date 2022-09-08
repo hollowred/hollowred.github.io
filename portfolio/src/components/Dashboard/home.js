e6000c4da83d86c8acd28b68a0dc325072debc25
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-08 14:00:04 -0500

    placeholder corrections

--- portfolio/src/components/Dashboard/home.js
+++ portfolio/src/components/Dashboard/home.js
@@ -58,10 +58,10 @@ const Home = () => {
         <div className="dashboard">
 
             <form ref={form} onSubmit={submitPortfolio}>
-                <p><input type="text" placehplder="Name" /></p>
-                <p><textarea placehplder="Description" /></p>
-                <p><input type="text" placehplder="Url" /></p>
-                <p><input type="file" placehplder="Image" /></p>
+                <p><input type="text" placeholder="Name" /></p>
+                <p><textarea placeholder="Description" /></p>
+                <p><input type="text" placeholder="Url" /></p>
+                <p><input type="file" placeholder="Image" /></p>
                 <button type="submit">Submit</button>
                 <button onClick={() => auth.signOut()}>Sign out</button>
             </form>
