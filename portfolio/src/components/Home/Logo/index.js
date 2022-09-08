37689a6ad07fea8748a2839e3c02a1b2d2471a59
Salem Iyabode <Shelton@Sheltons-MacBook-Pro.local>
2022-09-08 13:19:40 -0500

    delete gsap

--- portfolio/src/components/Home/Logo/index.js
+++ portfolio/src/components/Home/Logo/index.js
@@ -1,7 +1,6 @@
 import { useEffect, useRef } from 'react'
 import './index.scss';
 import LogoS from '../../../assets/images/logo-s.png'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
 import gsap from 'gsap-trial'
 
 
@@ -10,30 +9,7 @@ const Logo =() => {
     const outlineLogoRef = useRef()
     const solidLogoRef = useRef()
 
-    useEffect(() =>{
-        gsap.registerPlugin(DrawSVGPlugin)
-
-        gsap.timeline()
-        .to(bgRef.current, {
-            duraion: 1,
-            opacity: 1,
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
-
-        gsap.fromTo(
-            solidLogoRef.current,
-            {
-                opacity: 0,
-            },
-            {
-                opacity: 1,
-                delay: 4,
-                duration: 4,
-            }
-        )
+    useEffect(() =>{     
     }, [])
 
 
